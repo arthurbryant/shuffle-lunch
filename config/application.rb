58e6a81f2a6b9f4c5723003578acd7d1d241e5a7
feng.zhang <cst.feng@gmail.com>
2018-10-12 16:42:48 +0900

    autoload services

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module ShuffleLunch
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
+    config.autoload_paths += %W(#{config.root}/services)
+    config.autoload_paths += Dir["#{config.root}/services/**/"]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
